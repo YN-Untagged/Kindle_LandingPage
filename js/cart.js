f6896897f1cb468d0724d3cb65eb1dd43bc56f0b
YN-Untagged <89791536+YN-Untagged@users.noreply.github.com>
2022-05-26 17:00:04 +0200

    Cart UI Update

--- js/cart.js
+++ js/cart.js
@@ -22,13 +22,13 @@ function ShowBookDetails(id){
     modalDetails.show();
 }
 
-function IncreaseQuantity(id){
-    const input = document.getElementById(id);
+function IncreaseQuantity(){
+    const input = document.getElementById('quantity');
     input.value = parseInt(input.value) + 1;
 }
 
-function DecreaseQuantity(id){
-    const input = document.getElementById(id);
+function DecreaseQuantity(){
+    const input = document.getElementById('quantity');
     input.value = parseInt(input.value) - 1;
 }
 
@@ -74,23 +74,105 @@ aform.addEventListener('submit', function(e){
 
 function LoadCart(){
 
+    if(!"user" in sessionStorage){
+        window.location.href = "login.html";
+    }
+
     LoadNavigators();
     LoadUserDetails();
+    ShowHideSideNav();
 
     let users = RetrieveUsers(),
     index = sessionStorage.getItem('userIndex'),
-    html = "",
-    sum = 0;
-    let userCart = users[index]["cart"];
-    userCart.forEach(item => {
-        html += `<li>${books[item.bookId]["name"]} R${books[item.bookId]["price"]} Qty: ${item.quantity}</li>`;
-        sum += books[item.bookId]["price"] * item.quantity;
-    });
-    html += `<p> total: ${sum} </p>`;
-    document.getElementById('cart-container').innerHTML = html;
+    sum = 0,
+    cartList = document.getElementById('cart-container'),
+    userCart = users[index]["cart"];
+    var total = document.createElement('div'),
+    backTo = `<a href="index.html"><i class="fas fa-long-arrow-alt-left"></i>Continue shopping</a>`;
+    total.className = "list-group-item justify-content-between d-flex align-items-center";
 
+    if(userCart.length > 0)
+    {
+        userCart.forEach(item => {
+            var cartItem = AddListItem(item);
+            cartList.append(cartItem);
+            sum += books[item.bookId]["price"] * item.quantity;
+        });
+    
+        
+        total.innerHTML = `${backTo}<div> Subtotal: R ${sum} </div>`;
+        
+    }
+    else{
+        total.innerHTML = `<div><h3>Your cart is empty</h3> ${backTo} <div>`;
+    }
+    cartList.append(total);
+    
 }
 
-function RemoveFromCart(item){
+function UpdateCart(id, rmvd){
 
+    let users = RetrieveUsers(),
+    qty = document.getElementById('quantity').value;
+    index = sessionStorage.getItem('userIndex');
+
+    users[index]["cart"].forEach((item, indx) => {
+        if(item.bookId === id){
+            item.quantity = qty;
+            if(rmvd || qty <= 0){
+                delete users[indx]["cart"][indx];
+            }
+            return;
+        }
+    });
+
+    StoreUsers(users);
+    window.reload();
+}
+
+function ChangeQuantity(e, form){
+    e.preventDefault();
+    if(e.target.value === "+"){
+        alert(e.target.value);
+        alert(parseInt(form['quantity'].value) + 1);
+    }
+    else{
+        alert(e.target.value);
+        alert(parseInt(form['quantity'].value) - 1);
+    }
+    
+}
+
+function AddListItem(item){
+
+    var listItem = document.createElement('div');
+    listItem.className = "list-group-item justify-content-between d-flex align-items-center";
+    listItem.innerHTML = `
+        <div class="col-7">
+            <div class="row align-items-center">
+                <img class="cart_book" src="${booksUrl + books[item.bookId]['cover']}">
+                <div>
+                    <h6>${books[item.bookId]["name"]}</h6>
+                    <small>${books[item.bookId]["author"]}</small>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <form onclick="ChangeQuantity(event, this);">
+                <div class="btn-group">
+                    <input type="submit" name="subtract" value="-">
+                    <input name="quantity" type="number" class="modal_input" min="0" value="${item.quantity}" required>
+                    <input type="submit" name="add" value="+">
+                </div>
+            </form>
+        </div>
+        <div class="col-2">
+            <div class="row justify-content-between">
+                <span>R ${books[item.bookId]["price"]}</span>
+                <a onclick="UpdateCart(${item.bookId}, true);"><i class="fas fa-times"></i></a>
+            </div>
+        </div>
+    `;
+
+    return listItem;
 }
